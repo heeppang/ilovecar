c278467c6f582df1d925a22e89896a985975047a
HeebbangDuck <97587597+heeppang@users.noreply.github.com>
2022-05-02 18:32:18 +0900

    Add files via upload

---
+++ pybo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Station
+
+# Register your models here.
+
+# admin.site.register(Question)
+# admin.site.register(Answer)
+admin.site.register(Station)
